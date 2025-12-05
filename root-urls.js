b1c7f76095bd6a2a7341cc9a1b47a3c41531e82a
Nguyễn Hữu Quý <22485887+nguyenquy0710@users.noreply.github.com>
2025-12-05 15:36:38 +0000

    feat: add initial project structure with GitHub workflows, funding, and documentation

---
+++ root-urls.js
@@ -0,0 +1,15 @@
+// root-urls.js
+const urls = [
+  {
+    group: "🛠️ Online Tools",
+    group_id: "online_tools",
+    links: [
+      {
+        name: "YouTube miễn phí",
+        path: "https://www.clipto.com/vi/media-downloader/youtube-downloader",
+        title: "Trình tải xuống video YouTube miễn phí",
+        target: '_blank',
+      },
+    ]
+  },
+];
