2cbf2662cf7761813a2a3b56edb7130cee3660c6
ltk1005 <ltk1005@users.noreply.github.com>
2026-02-03 01:47:11 +0000

    deploy: 7eb2a9e410c4139e8eefaaa8ae861d2106cbc6a0

---
+++ root-urls.js
@@ -0,0 +1,45 @@
+// root-urls.js
+const urls = [
+  {
+    group: "🛠️ Online Tools",
+    group_id: "online_tools",
+    links: [
+      {
+        name: "YouTube miễn phí",
+        path: "https://www.clipto.com/vi/media-downloader/youtube-downloader",
+        title: "Trình tải xuống video YouTube miễn phí",
+        target: '_blank',
+      },
+      {
+        name: "Trình tải xuống video YouTube",
+        path: "https://yt5s.rip/vi11/",
+        title: "Trình tải xuống video YouTube trực tuyến miễn phí tại yt5s.best",
+        target: '_blank',
+      },
+      {
+        name: "Tải xuống video Tiktok (Douyin)",
+        path: "https://snaptik.vn/",
+        title: "Tải xuống video Tiktok (Douyin) không có watermark miễn phí",
+        target: '_blank',
+      },
+      {
+        name: "Tải Video TikTok HD Không Logo, Download Mp3 Miễn Phí",
+        path: "https://snapsave.vn/tiktok",
+        title: "Tải Video TikTok HD Không Logo, Download Mp3 Miễn Phí",
+        target: '_blank',
+      },
+      {
+        name: "Tải video Facebook",
+        path: "https://vi.clipsave.net/",
+        title: "Cách tải xuống video và Reels Facebook một cách dễ dàng",
+        target: '_blank',
+      },
+      {
+        name: "Download video",
+        path: "https://downloadvideo.vn/vi",
+        title: "Tải video từ nhiều nền tảng như YouTube, Facebook, TikTok...",
+        target: '_blank',
+      },
+    ],
+  },
+];
